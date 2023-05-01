2c211f5db4fa47c387097f413d7f6b605fa481aa
Vinu Cyril <vinucyril@gmail.com>
2023-05-01 21:45:11 +0530

    add cart design

--- src/Addcart.js
+++ src/Addcart.js
@@ -1,6 +1,7 @@
 import "./Main.css";
 import minus from "./icon-minus.svg";
 import plus from "./icon-plus.svg";
+import cart from "./icon-cart.svg";
 
 const Addcart = () => {
   return (
@@ -12,6 +13,12 @@ const Addcart = () => {
         <span className="button-span">0</span>
         <button>
           <img src={plus}></img>
+        </button>
+      </div>
+      <div className="btn">
+        <button className="add-button">
+          <img style={{ color: "#fff", paddingRight: "2rem" }} src={cart}></img>
+          Add to cart
         </button>
       </div>
     </div>
